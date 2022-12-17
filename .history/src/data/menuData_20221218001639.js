ffc2cd40315bf7068ca293bbbd006ba48b54fffd
Pix3L Click3r <blancaflorkevin@gmail.com>
2022-12-18 00:17:04 +0100

    Development: Nav

--- .history/src/data/menuData_20221218001639.js
+++ .history/src/data/menuData_20221218001639.js
@@ -9,7 +9,7 @@ export const menuData = [
   { title: "", icon: "/images/icons/search.svg", link: "/search" },
   { title: "", icon: "/images/icons/account.svg", link: "/account" },
 ]
-{/*
+/*
 export const tooltipData = [
   { title: "Profile", icon: "/images/icons/profile.svg", link: "/profile" },
   { title: "Settings", icon: "/images/icons/settings.svg", link: "/settings" },
