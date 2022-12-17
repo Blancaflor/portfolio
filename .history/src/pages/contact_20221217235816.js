427b15ef43ab00f317a441f8ba95d008ee778429
Pix3L Click3r <blancaflorkevin@gmail.com>
2022-12-17 23:58:48 +0100

    Development: Nav Contact

--- .history/src/pages/contact_20221217235816.js
+++ .history/src/pages/contact_20221217235816.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React from "react"
 import emailjs from "emailjs-com"
 import Layout from "../components/layout/layout"
 import SEO from "../components/layout/seo"
