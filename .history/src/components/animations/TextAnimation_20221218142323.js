d727fd8de36f1eb6a0bf661c7f83296b505869b8
Pix3L Click3r <blancaflorkevin@gmail.com>
2022-12-18 14:24:14 +0100

    Development: Text Animation

--- .history/src/components/animations/TextAnimation_20221218142323.js
+++ .history/src/components/animations/TextAnimation_20221218142323.js
@@ -9,9 +9,9 @@ export default function TextAnimation() {
 
 const animation = keyframes`
   0% { opacity: 0; transform: translateY(-100px) skewY(10deg) skewX(10deg) rotateZ(30deg); filter: blur(10px); }
-  25% { opacity: 1; transform: translateY(0px) skewY(0deg) skewX(0deg) rotateZ(0deg); filter: blur(0px); }
-  75% { opacity: 1; transform: translateY(0px) skewY(0deg) skewX(0deg) rotateZ(0deg); filter: blur(0px); }
-  100% { opacity: 0; transform: translateY(-100px) skewY(10deg) skewX(10deg) rotateZ(30deg); filter: blur(10px); }
+  25% { opacity: 1; transform: translateY(0px) skewY(0deg) skewX(10deg) rotateZ(0deg); filter: blur(0px); }
+  75% { opacity: 1; transform: translateY(0px) skewY(0deg) skewX(10deg) rotateZ(0deg); filter: blur(0px); }
+  100% { opacity: 0; transform: translateY(-100px) skewY(10deg) rotateZ(30deg); filter: blur(10px); }
 `
 
 const Wrapper = styled.span`
