37b6c6c9ee2f5b1e83c72141f3fe72f1e2868a7c
Pix3L Click3r <blancaflorkevin@gmail.com>
2022-12-18 15:30:31 +0100

    Development: Contact Page

--- .history/src/pages/contact_20221218153011.js
+++ .history/src/pages/contact_20221218153011.js
@@ -6,8 +6,8 @@ import styled, { keyframes } from "styled-components"
 import WaveBackground from "../components/backgrounds/WaveBackground"
 import { themes } from "../components/styles/ColorStyles"
 import { H1, MediumText } from "../components/styles/TextStyles"
-import TextAnimation from "../components/animations/TextAnimation"
-import TextAnimation2 from "../components/animations/TextAnimation2"
+import TextAnimation from "../components//animations/TextAnimation"
+import TextAnimation2 from "../animations/TextAnimation2"
 
 export default function Contact() {
 
