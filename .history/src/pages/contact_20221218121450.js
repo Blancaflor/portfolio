9a48ffdea75ded821b73438b3741fea3bb7cb6cd
Pix3L Click3r <blancaflorkevin@gmail.com>
2022-12-18 12:15:07 +0100

    Development: Form Settings

--- .history/src/pages/contact_20221218121450.js
+++ .history/src/pages/contact_20221218121450.js
@@ -4,7 +4,7 @@ import Layout from "../components/layout/layout"
 import SEO from "../components/layout/seo"
 import styled, { keyframes } from "styled-components"
 
-import WaveBackground from "../components/backgrounds/WaveBackground"
+import WaveBackground from "./components/backgrounds/WaveBackground"
 import PurchaseButton from "../components/buttons/PurchaseButton"
 import { themes } from "../components/styles/ColorStyles"
 import { H1, MediumText } from "../components/styles/TextStyles"
