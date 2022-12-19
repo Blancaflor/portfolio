fd0895a790abb2229f0739521cc410685710e170
Pix3L Click3r <blancaflorkevin@gmail.com>
2022-12-19 22:45:25 +0100

    Dev

--- .history/src/components/sections/HeroSection_20221219224429.js
+++ .history/src/components/sections/HeroSection_20221219224429.js
@@ -56,9 +56,9 @@ const ContentWrapper = styled.div`
     padding: 150px 20px 250px;
   }
   @media (max-width: 1920px) {
-    grid-template-columns: 360px auto;
-    gap: 80px;
-    padding: 250px 0 250px;
+    grid-template-columns: 2px auto;
+    gap: 60px;
+    padding: 150px 0 250px;
   }
   @media (max-width: 1600px) {
     grid-template-columns: 320px auto;
--- .history/src/components/sections/HeroSection_20221219224430.js
+++ .history/src/components/sections/HeroSection_20221219224430.js
@@ -57,8 +57,8 @@ const ContentWrapper = styled.div`
   }
   @media (max-width: 1920px) {
     grid-template-columns: 360px auto;
-    gap: 80px;
-    padding: 250px 0 250px;
+    gap: 60px;
+    padding: 150px 0 250px;
   }
   @media (max-width: 1600px) {
     grid-template-columns: 320px auto;
