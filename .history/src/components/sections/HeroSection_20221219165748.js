7a45421c5f3015f3d6de80d522ef155642851739
Pix3L Click3r <blancaflorkevin@gmail.com>
2022-12-19 16:58:07 +0100

    Dev

--- .history/src/components/sections/HeroSection_20221219165748.js
+++ .history/src/components/sections/HeroSection_20221219165748.js
@@ -76,7 +76,6 @@ const ContentWrapper = styled.div`
     padding: 150px 20px 250px;
   }
   @media (max-width: 1280px) {
-    width: 75%;
     grid-template-columns: 150px auto;
     gap: 60px;
     padding: 150px 20px 250px;
--- .history/src/components/sections/HeroSection_20221219165755.js
+++ .history/src/components/sections/HeroSection_20221219165755.js
@@ -76,7 +76,7 @@ const ContentWrapper = styled.div`
     padding: 150px 20px 250px;
   }
   @media (max-width: 1280px) {
-    width: 75%;
+    wid
     grid-template-columns: 150px auto;
     gap: 60px;
     padding: 150px 20px 250px;
