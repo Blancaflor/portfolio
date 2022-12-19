95b64e5136a895d3055e904f33ada1f39c601d62
Pix3L Click3r <blancaflorkevin@gmail.com>
2022-12-19 14:50:34 +0100

    Dev

--- .history/src/components/sections/HeroSection_20221219145028.js
+++ .history/src/components/sections/HeroSection_20221219145028.js
@@ -82,7 +82,7 @@ const ContentWrapper = styled.div`
   @media (max-width: 338px) {
     grid-template-columns: auto;
     gap: 60px;
-    max-width: 150px;
+    max-width: px;
     padding: 150px 20px 250px;
   }
 `
