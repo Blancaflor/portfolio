bd2eccae2882eb5714da33de50e088ac5887f341
Pix3L Click3r <blancaflorkevin@gmail.com>
2022-12-20 12:28:24 +0100

    Dev

--- .history/src/pages/contact_20221220122738.js
+++ .history/src/pages/contact_20221220122738.js
@@ -122,7 +122,7 @@ const ContentWrapper = styled.div`
   }
 
   @media (max-width: 540px) {
-    grid-template-rows: auto;
+    grid-template-columns: auto;
     gap: 60px;
     padding: 150px 20px 250px;
   }
