d2a39d4066842d3ce25249a76862055c7c03b167
Pix3L Click3r <blancaflorkevin@gmail.com>
2022-12-20 18:38:56 +0100

    Dev

--- .history/gatsby-config_20221220183718.js
+++ .history/gatsby-config_20221220183718.js
@@ -19,8 +19,8 @@ module.exports = {
       resolve: "gatsby-source-contentful",
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
-      }
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
     },
     "gatsby-plugin-sass",
     // this plugin will pull all the files in our project system
