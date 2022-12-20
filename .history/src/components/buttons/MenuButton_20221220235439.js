537e11028e420b671807dda4ba07a1b3f9b75afd
Pix3L Click3r <blancaflorkevin@gmail.com>
2022-12-20 23:54:46 +0100

    Dev

--- .history/src/components/buttons/MenuButton_20221220235439.js
+++ .history/src/components/buttons/MenuButton_20221220235439.js
@@ -24,6 +24,8 @@ const MenuItem = styled.div`
   transition: 0.5s ease-out;
   border-radius: 10px;
 
+
+
   :hover {
     background: rgba(255, 255, 255, 0.1);
     box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.1), 
