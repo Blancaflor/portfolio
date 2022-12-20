e245f9fad00864dc841b2eda7d59ec79bfbd01ab
Pix3L Click3r <blancaflorkevin@gmail.com>
2022-12-20 19:14:08 +0100

    Dev

---
+++ .history/gatsby-config_20221220191037.js
@@ -0,0 +1,50 @@
+module.exports = {
+  siteMetadata: {
+    title: `Next Gen Solutions| NGS | Official Website`,
+    description: `We design and code websites for your company, everything involved with web development and digital design.`,
+    author: `@Blancaflor`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/static/images`,
+      },
+    },
+    `gatsby-plugin-styled-components`,
+    "gatsby-plugin-sharp",
+    // REMARK plugin needed to extract the markdown files and parses
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          "gatsby-remark-relative-images",
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 750,
+              linkImagesOriginal: false
+            }
+          }
+        ]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site./ This path is relative to the root of the site.
+      }
+    }
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
