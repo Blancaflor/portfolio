0d86684d89bb2bbca1f0e45103c89acd2da86c9e
Pix3L Click3r <blancaflorkevin@gmail.com>
2023-01-08 13:22:15 +0100

    Dev

---
+++ .history/src/components/animations/AboutIcons_20230108132203.js
@@ -0,0 +1,229 @@
+import React from "react"
+import styled from "styled-components"
+import { Link } from "gatsby"
+
+export default function AboutIcons() {
+  return (
+    <Wrapper>
+      <Icon1>
+        <Link to="#" target="_blank">
+          <div className="icon1" />
+        </Link>
+      </Icon1>
+      <Icon2>
+        <Link to="#" target="_blank">
+          <div className="icon2" />
+        </Link>
+      </Icon2>
+      <Icon3>
+        <Link to="#" target="_blank">
+          <div className="icon3" />
+        </Link>
+      </Icon3>
+      <Icon4>
+        <Link to="#" target="_blank">
+          <div className="icon4" />
+        </Link>
+      </Icon4>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  position: relative;
+  perspective: 5000;
+  top: 60px;
+  height: 100px;
+  display: grid;
+  grid-template-columns: 20px 20px;
+  grid-auto-columns: 1fr;
+  gap: 50px;
+  padding: 20px;
+  width: 100%;
+  justify-content: space-around;
+  align-items: left;
+
+  @media (max-width: 1600px) {
+    transform: scale(0.9);
+  }
+
+  @media (max-width: 1440px) {
+    transform: scale(0.9);
+  }
+
+  @media (max-width: 1280px) {
+    transform: scale(0.8);
+  }
+
+  @media (max-width: 960px) {
+    transform: scale(0.8);
+    transform-origin: top left;
+  }
+
+  @media (max-width: 768px) {
+    transform: scale(0.8);
+    transform-origin: top left;
+  }
+
+  @media (max-width: 540px) {
+    transform: scale(0.7);
+  }
+
+  @media (max-width: 450px) {
+    transform: scale(0.7);
+  }
+
+  @media (max-width: 338px) {
+    transform: scale(0.6);
+  }
+
+  .icon1 {
+    position: absolute;
+    width: 60px;
+    height: 60px;
+    top: 350px;
+    background: url("/images/animations/html.png"), 
+    rgba(23, 12, 61, 0.5);
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    box-shadow: 0px 8.19119px 16.3824px rgba(0, 0, 0, 0.1),
+    backdrop-filter: blur(21.8507px);
+    /* Note: backdrop-filter has minimal browser support */
+    border-radius: 50%;
+  }
+
+  .icon2 {
+    position: absolute;
+    width: 60px;
+    height: 60px;
+    top: 350px;
+    background: url("/images/animations/css.png")
+    , 
+    rgba(23, 12, 61, 0.5);
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    box-shadow: 0px 8.19119px 16.3824px rgba(0, 0, 0, 0.1),
+    backdrop-filter: blur(21.8507px);
+    /* Note: backdrop-filter has minimal browser support */
+    border-radius: 50%;
+  }
+
+  .icon3 {
+    position: absolute;
+    width: 60px;
+    height: 60px;
+    top: 540px;
+    background: url("/images/animations/js.png")
+    , 
+    rgba(23, 12, 61, 0.5);
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    box-shadow: 0px 8.19119px 16.3824px rgba(0, 0, 0, 0.1),
+    backdrop-filter: blur(21.8507px);
+    /* Note: backdrop-filter has minimal browser support */
+    border-radius: 50%;
+  }
+
+  .icon4 {
+    position: absolute;
+    width: 60px;
+    height: 60px;
+    top: 540px;
+    background: url("/images/animations/react.png")
+    , 
+    rgba(23, 12, 61, 0.5);
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    box-shadow: 0px 8.19119px 16.3824px rgba(0, 0, 0, 0.1),
+    backdrop-filter: blur(21.8507px);
+    /* Note: backdrop-filter has minimal browser support */
+    border-radius: 50%;
+  }
+`
+
+const Icon1 = styled.div`
+  div {
+    transform: rotateY(-10deg) rotateX(10deg);
+    transform-origin: bottom left;
+  }
+
+  * {
+    transition: 1s cubic-bezier(0.075, 0.82, 0.165, 1);
+  }
+  :hover div {
+    transform: rotateY(0deg) rotateX(0deg);
+    &.mockup1 {
+      transition-delay: 0.1s;
+      transform: translate(-30px, -30px);
+    }
+    :hover {
+      filter: brightness(150%) saturate(120%);
+    }
+  }
+`
+
+const Icon2 = styled.div`
+  div {
+    transform: rotateY(-10deg) rotateX(10deg);
+    transform-origin: bottom left;
+  }
+
+  * {
+    transition: 1s cubic-bezier(0.075, 0.82, 0.165, 1);
+  }
+  :hover div {
+    transform: rotateY(0deg) rotateX(0deg);
+    &.mockup2 {
+      transition-delay: 0s;
+      transform: translate(0px, -30px);
+    }
+    :hover {
+      filter: brightness(150%) saturate(120%);
+    }
+  }
+`
+
+const Icon3 = styled.div`
+  div {
+    transform: rotateY(-10deg) rotateX(10deg);
+    transform-origin: bottom left;
+  }
+
+  * {
+    transition: 1s cubic-bezier(0.075, 0.82, 0.165, 1);
+  }
+  :hover div {
+    transform: rotateY(0deg) rotateX(0deg);
+    &.mockup3 {
+      transition-delay: 0s;
+    }
+    :hover {
+      filter: brightness(150%) saturate(120%);
+    }
+  }
+`
+
+const Icon4 = styled.div`
+  div {
+    transform: rotateY(-10deg) rotateX(10deg);
+    transform-origin: bottom left;
+  }
+
+  * {
+    transition: 1s cubic-bezier(0.075, 0.82, 0.165, 1);
+  }
+  :hover div {
+    transform: rotateY(0deg) rotateX(0deg);
+    &.mockup4 {
+      transition-delay: 0.1s;
+      transform: translate(-120px, 30px);
+    }
+    :hover {
+      filter: brightness(150%) saturate(120%);
+    }
+  }
+`
