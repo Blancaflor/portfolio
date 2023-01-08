9bb868c7886ddcbaf93eb3b2331375d532844993
Pix3L Click3r <blancaflorkevin@gmail.com>
2023-01-08 14:29:36 +0100

    Dev

--- .history/src/pages/about_20230108142929.js
+++ .history/src/pages/about_20230108142929.js
@@ -95,7 +95,7 @@ const ContentWrapper = styled.div`
   }
   
   @media (max-width: 1920px) {
-    grid-template-columns: 360px auto;
+    grid-template-columns: 30px auto;
     gap: 80px;
     padding: 250px 0 250px;
   }
