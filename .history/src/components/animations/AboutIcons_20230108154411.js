845c232231b42aecd23a08396bed7a452b2aa263
Pix3L Click3r <blancaflorkevin@gmail.com>
2023-01-08 15:44:21 +0100

    Dev

--- .history/src/components/animations/AboutIcons_20230108154411.js
+++ .history/src/components/animations/AboutIcons_20230108154411.js
@@ -79,8 +79,8 @@ const Wrapper = styled.div`
 
   .icon1 {
     position: absolute;
-    width: 60px;
-    height: 60px;
+    width: 8px;
+    height: 8px;
     top: 470px;
     background: url("/images/animations/html.png"), 
     rgba(23, 12, 61, 0.5);
@@ -95,8 +95,8 @@ const Wrapper = styled.div`
 
   .icon2 {
     position: absolute;
-    width: 60px;
-    height: 60px;
+    width: 8px;
+    height: 8px;
     top: 470px;
     background: url("/images/animations/css.png")
     , 
@@ -112,8 +112,8 @@ const Wrapper = styled.div`
 
   .icon3 {
     position: absolute;
-    width: 60px;
-    height: 60px;
+    width: 8px;
+    height: 8px;
     top: 660px;
     background: url("/images/animations/js.png")
     , 
@@ -129,8 +129,8 @@ const Wrapper = styled.div`
 
   .icon4 {
     position: absolute;
-    width: 60px;
-    height: 60px;
+    width: 8px;
+    height: 8px;
     top: 660px;
     background: url("/images/animations/react.png")
     , 
