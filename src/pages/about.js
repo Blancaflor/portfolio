b6983b77c07e664e4e49910d0ed6ce71bb65a522
Pix3L Click3r <blancaflorkevin@gmail.com>
2023-01-11 03:56:15 +0100

    Dev

--- src/pages/about.js
+++ src/pages/about.js
@@ -277,7 +277,7 @@ const TextBackGround = styled.div`
   box-shadow: 0px 8.19119px 16.3824px rgba(0, 0, 0, 0.1),
   backdrop-filter: blur(21.8507px);
   border-radius: 16px;
-  padding: 0 20px 0 20px;
+  padding: 0 20px 0 0px;
   opacity: 90%;
   height: 810px;
 `
