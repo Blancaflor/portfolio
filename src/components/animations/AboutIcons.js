797d7cb0f114c64a0a3270472e0c16776cba3c53
Pix3L Click3r <blancaflorkevin@gmail.com>
2023-01-12 07:26:09 +0100

    Dev

--- src/components/animations/AboutIcons.js
+++ src/components/animations/AboutIcons.js
@@ -37,19 +37,7 @@ const Wrapper = styled.div`
   width: 100%;
   justify-content: space-around;
   align-items: left;
-
-  :hover {
-    box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1),
-    0px 30px 60px rgba(23, 0, 102, 0.5),
-    inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.5);
-    transform: translateY(-3px);
-
-    .icon {
-        transform: scale(1.2);
-        transition: 1s cubic-bezier(0.075, 0.82, 0.165, 1);
-    }
-  }
-
+  
   h1{
     color: white;
     font-weight: bold;
@@ -124,6 +112,22 @@ const IconWrapper = styled.div`
   align-content: center;
   justify-self: center;
   position: relative;
+
+  *,
+  & {
+    transition: 1s cubic-bezier(0.075, 0.82, 0.165, 1);
+  }
+
+  :hover {
+    box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1),
+    0px 30px 60px rgba(23, 0, 102, 0.5),
+    inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.5);
+    transform: translateY(-3px);
+
+    .icon {
+        transform: scale(1.2);
+    }
+  }
 
   ${Wrapper}:hover & {
     filter: hue-rotate(10deg) brightness(150%) saturate(120%);
