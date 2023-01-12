86e4d302eb9b05b68e42a2574aa7be06f585851a
Pix3L Click3r <blancaflorkevin@gmail.com>
2023-01-12 08:50:57 +0100

    Dev

--- src/components/animations/AboutIcons.js
+++ src/components/animations/AboutIcons.js
@@ -117,7 +117,7 @@ const IconWrapper = styled.div`
   align-content: center;
   justify-self: center;
   position: relative;
-  margin: 20px 0 30px auto;
+  margin: 20px 0 30px 150px;
 
   *,
   & {
