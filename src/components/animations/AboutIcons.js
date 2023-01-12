44be3bccc88e92bb17d587828a2510947ede8923
Pix3L Click3r <blancaflorkevin@gmail.com>
2023-01-12 08:59:51 +0100

    Dev

--- src/components/animations/AboutIcons.js
+++ src/components/animations/AboutIcons.js
@@ -117,7 +117,51 @@ const IconWrapper = styled.div`
   align-content: center;
   justify-self: center;
   position: relative;
-  margin: 60px 0 30px 150px;
+  margin: 80px 0 30px 150px;
+
+  @media (max-width: 2560px) {
+    padding: 250px 20px 250px;
+  }
+  
+  @media (max-width: 1920px) {
+    padding: 250px 0 250px;
+  }
+
+  @media (max-width: 1600px) {
+    padding: 150px 20px 250px;
+  }
+
+  @media (max-width: 1440px) {
+    padding: 150px 20px 250px;
+  }
+
+  @media (max-width: 1360px) {
+    padding: 150px 20px 250px;
+  }
+
+  @media (max-width: 1280px) {
+    padding: 150px 20px 250px;
+  }
+
+  @media (max-width: 960px) {
+    padding: 150px 20px 250px;
+  }
+
+  @media (max-width: 720px) {
+    padding: 150px 20px 250px;
+  }
+
+  @media (max-width: 540px) {
+    padding: 150px 20px 250px;
+  }
+
+  @media (max-width: 450px) {
+    padding: 150px 20px 250px;
+  }
+
+  @media (max-width: 338px) {
+    padding: 100px 20px 250px;
+  }
 
   *,
   & {
