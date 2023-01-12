3ffe5191c9dcf06051d1adea85286d39960796d5
Pix3L Click3r <blancaflorkevin@gmail.com>
2023-01-12 10:19:11 +0100

    Dev

--- src/components/animations/AboutIcons.js
+++ src/components/animations/AboutIcons.js
@@ -187,7 +187,7 @@ const IconWrapper = styled.div`
 const Ring = styled.img`
   position: absolute;
   top: -32px;
-  left: -26px;
+  left: -27px;
   width: 128px;
   height: 128px;
   display: grid;
