e33cd49381f26fba379ba4551c71b0b13e0a5f80
Pix3L Click3r <blancaflorkevin@gmail.com>
2023-01-13 12:36:55 +0100

    Dev

--- src/pages/about.js
+++ src/pages/about.js
@@ -130,7 +130,7 @@ const ContentWrapper = styled.div`
   @media (max-width: 1920px) {
     grid-template-columns: 360px auto;
     gap: 80px;
-    padding: 250px 20px 240px;
+    padding: 250px 20px 230px;
   }
 
   @media (max-width: 1600px) {
