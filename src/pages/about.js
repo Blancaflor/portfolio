44ca67c905fa252b5ee6490b8f84c05be577d91d
Pix3L Click3r <blancaflorkevin@gmail.com>
2023-01-13 06:21:59 +0100

    Dev

--- src/pages/about.js
+++ src/pages/about.js
@@ -190,6 +190,7 @@ const ContentWrapper2 = styled.div`
   padding: 200px 30px;
   display: grid;
   grid-template-columns: 360px auto;
+  justify-content: center;
 
   @media (max-width: 2560px) {
     grid-template-columns: 400px auto;
@@ -332,50 +333,6 @@ const TextBackGround = styled.div`
     font-size: 90%;
     text-align: center;
     margin-top: 10px;
-  }
-
-  @media (max-width: 2560px) {
-    padding: 80px 0 30px 150px;
-  }
-  
-  @media (max-width: 1920px) {
-    padding: 80px 0 30px 130px;
-  }
-
-  @media (max-width: 1600px) {
-    padding: 80px 0 30px 130px;
-  }
-
-  @media (max-width: 1440px) {
-    padding: 80px 0 30px 110px;
-  }
-
-  @media (max-width: 1360px) {
-    padding: 80px 0 30px 100px;
-  }
-
-  @media (max-width: 1280px) {
-    padding: 80px 0 30px 90px;
-  }
-
-  @media (max-width: 960px) {
-    padding: 80px 0 30px 50px;
-  }
-
-  @media (max-width: 720px) {
-    padding: 80px 0 30px 30px;
-  }
-
-  @media (max-width: 540px) {
-    padding: 80px 0 30px 10px;
-  }
-
-  @media (max-width: 450px) {
-    padding: 80px 0 30px 0;
-  }
-
-  @media (max-width: 338px) {
-    padding: 80px 0 30px -10px;
   }
 `
 
