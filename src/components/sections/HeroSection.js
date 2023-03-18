8bb9033a62c019bf78163d1a4526828d0f991a02
Pix3L Click3r <blancaflorkevin@gmail.com>
2023-03-18 13:45:18 +0100

    Dev

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -25,7 +25,7 @@ function HeroSection() {
           <Description>Welcome on The Digital Platform</Description>
           <PurchaseButton
             title="Make An Appointment"
-            subtitle="Send us an email!"
+            subtitle="Send us an email! Test"
           />
         </TextWrapper>
         <MockupAnimation />
